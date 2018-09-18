5b56afe0ac703dd4cdf811b7e9c3e61775ec1770
justdealman <justdealman@gmail.com>
2018-09-18 15:17:07 +0700

    card buy button fixed

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -37,7 +37,7 @@ $(function() {
 	var justSwitched = false;
 	function detectDevice() {
 		var temp = isMobile;
-		if ( Modernizr.mq('(max-width:999px)') ) {
+		if ( Modernizr.mq('(max-width:991px)') ) {
 			isMobile = true;
 		} else {
 			isMobile = false;
@@ -76,6 +76,20 @@ $(function() {
 		var links = $(this).parents('[data-tabs-container]').find('[data-tabs-nav]');
 		links.find('a[href="'+id+'"]').addClass('is-active').siblings().removeClass('is-active');
 	});
+	
+	function setMinCard() {
+		if ( !isMobile ) {
+			var min = $(window).height()-$('.card').offset().top;
+			if ( min < 800 ) {
+				min = 800;
+			}
+		} else {
+			var min = 0;
+		}
+		$('.card__row').css({
+			'min-height': min+'px'
+		});
+	}
 
 	function startApp() {
 		detectDevice();
@@ -83,9 +97,13 @@ $(function() {
 			if ( isMobile ) {
 				
 			} else {
+				
 			}
 		}
 		setRatio();
+		if ( $('.card').length ) {
+			setMinCard();
+		}
 	}
 	
 	startApp();
@@ -168,5 +186,46 @@ $(function() {
 	}
 	$('.panel--close').on('click', function() {
 		panelClose();
+	});
+	
+	function setPriceBg(e) {
+		var t = e.parents('.card__button');
+		var buy = t.find('.button');
+		e.css({
+			left: buy.position().left,
+			top: buy.position().top,
+			width: buy.outerWidth(),
+			height: buy.outerHeight()
+		}).addClass('is-visible');
+	}
+	$('.card__button--bg').each(function() {
+		setPriceBg($(this));
+	});
+	$('.card__button').on('mouseenter', function() {
+		var bg = $(this).find('.card__button--bg');
+		bg.css({
+			left: 0,
+			top: 0,
+			width: 100+'%',
+			height: 100+'%'
+		});
+	});
+	$('.card__button').on('mouseleave', function() {
+		setPriceBg($(this).find('.card__button--bg'));
+	});
+	
+	$(document).on('scroll', function() {
+		var start = $('.card').offset().top+($('.card').outerHeight())-$(window).height();
+		var end = $('footer').offset().top-$(window).height();
+		if ( $(document).scrollTop() > start && !isMobile )  {
+			$('.card__controls').addClass('is-fixed');
+			if ( $(document).scrollTop() > end ) {
+				$('.card__controls').css({
+					marginBottom: $(document).scrollTop()-end
+				});
+			}
+		} else {
+			$('.card__controls').removeClass('is-fixed');
+		}
 	});
 });
